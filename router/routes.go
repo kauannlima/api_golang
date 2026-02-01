1c230b354076df336657bd44ba6171264e39e38b
kauannlima <kauanalmeidalima1405@gmail.com>
2026-02-01 14:11:46 -0300

    feat: enhance validation for CreateOpeningRequest and add UpdateOpeningRequest with validation

--- router/routes.go
+++ router/routes.go
@@ -2,15 +2,19 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	docs "github.com/kauannlima/api_golang/docs"
 	"github.com/kauannlima/api_golang/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitializeRoutes(router *gin.Engine) {
 
 	// Initialize handler
 	handler.InitializeHandler()
-
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -18,5 +22,7 @@ func InitializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
 	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 }
